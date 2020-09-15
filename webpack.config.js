a5066ddda24c713d80bffd38db6bf1a80f3426ea
tinfengyee <tinfengyee@163.com>
2020-09-16 00:32:26 +0800

    开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
@@ -12,9 +14,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
     // use full path. path.join(process.cwd(), 'build/**/*')
   },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 9000
+  },
   plugins: [
     new CleanWebpackPlugin({
       verbose: false,
+      cleanStaleWebpackAssets: false
     }),
     new HtmlWebpackPlugin({
       title: '管理输出',
