06fe80d7d0c71b6fd3c929b2005c9006d394103a
lih <v_buglh@outlook.com>
2020-09-15 18:00:30 +0800

    加载CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // lodash，现在通过一个 script 引入
   element.innerHTML = _.join(['Hello', 'weback'], ' ');
+  element.classList.add('hello')
 
   return element;
 }
